670f7e90e9215cb9daeaf4e7449df3e6c102da9f
William Jessop <willjessop12@gmail.com>
2022-12-01 19:14:29 -0700

    added basic lesson turn in

--- src/main/java/org/owasp/webgoat/lessons/custom/Custom123.java
+++ src/main/java/org/owasp/webgoat/lessons/custom/Custom123.java
@@ -9,15 +9,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.owasp.webgoat.container.session.UserSessionData;
 import org.springframework.beans.factory.annotation.Autowired;
+import java.net.*;
+import java.io.*;
 
 @RestController 
 // @AssignmentHints({"lesson-template.hints.1", "lesson-template.hints.2", "lesson-template.hints.3"}) 
 public class Custom123 extends AssignmentEndpoint { 
 
-private final String secretValue = "secr37Value";
+    private final String secretValue = "secr37Value";
 
     @Autowired
     private UserSessionData userSessionData; 
+
+    // Attempt for http lesson validation
+    // @GetMapping("/custom/log4shell1")
+    // public void setComplete(){
+    //     solved = true;
+    // }
 
     @PostMapping("/custom/assignment1") 
     @ResponseBody
@@ -28,12 +36,21 @@ private final String secretValue = "secr37Value";
         }
 
         //overly simple example for success. See other existing lessons for ways to detect 'success' or 'failure'
-        if (secretValue.equals(param1)) {
+        File tempFile = new File("/tmp/log4j-docker/filename.txt");
+        if (tempFile.exists()) {
             return success(this) 
                     .output("I did it")
                     .feedback("custom.assignment1.success")
                     .build();
             //lesson-template.sample-attack.success is defined in src/main/resources/i18n/WebGoatLabels.properties
+        }
+
+        try{
+            URL url = new URL("http://example.com");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+        }catch(Exception e){
+            // system.out.print("we died");
         }
 
         // else
